668875eb6844c22bb81ffa133ae9df1295e4a3b2
antonio-dojedul <anavarro67@hotmail.com>
2024-03-17 10:07:36 +0100

    fix restore main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import Home from './pages/Home/Home'
-import BookingButton from './components/BookingButton'
+
 
 const App = () => {
   return (
     <>
-    
-    
-    <Home> </Home>
-
-    <BookingButton
-
-      label="Enviar"
-      styles="orangeDesing"
-    
-    /> 
-
-    
-    
-    
-    
-    
-    
+    Hello
 </>
   
 )}
