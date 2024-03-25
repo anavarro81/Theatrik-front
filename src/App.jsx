ded38ee72329e9f06d312cb9760e39385197c15f
yeray <yeray.programacion.web@gmail.com>
2024-03-25 13:05:22 +0000

    empezando diseño filtros

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import BookingBtn from './components/atoms/Button/BookingBtn'
 import SearchBar from './components/molecules/SearchBar'
 import Header from './components/organisms/Header'
 import PlayCard from './components/molecules/PlayCard'
-import ContainerSlider from './components/organisms/Container/ContainerSlider'
+import ContainerSlider from './components/organisms/FilterBar'
 
 const App = () => {
   return (
