6efe4b4d8ad54a82fb32dbdfe7df46ba922722a5
yeray <yeray.programacion.web@gmail.com>
2024-03-26 22:48:52 +0000

    probando select

--- src/components/organisms/FilterBar.jsx
+++ src/components/organisms/FilterBar.jsx
@@ -1,85 +1,219 @@
-import React from "react";
-import classnames from "classnames";
-import BookingBtn from "../atoms/Button/BookingBtn";
-// import "../../css/FilterBar.css";
+// import React from "react";
+// import classnames from "classnames";
+// import BookingBtn from "../atoms/Button/BookingBtn";
+// // import "../../css/FilterBar.css";
 
-const FilterBar = (text, variant, icon) => {
-  const filterclasses = classnames("filterBar", {
-    "mx-5": true, //margin
-    "items-center": true,
-    "underline underline-offset-8": true,
-  });
+// const FilterBar = (text, variant, icon) => {
+//   const filterclasses = classnames("filterBar", {
+//     "mx-5": true, //margin
+//     "items-center": true,
+//     "underline underline-offset-8": true,
+//   });
 
-  const filterIcon = classnames(
-    "button relative flex flex-row items-center justify-items-center items-center justify-center border border-gray-300 rounded w-20 bg-gray-100",
-    {
-      // Agrega aquí cualquier clase condicional necesaria
-    }
-  );
+//   const filterIcon = classnames(
+//     "button relative flex items-center justify-items-center items-center justify-center border border-gray-300 rounded w-20 bg-gray-100",
+//     {
+//       // Agrega aquí cualquier clase condicional necesaria
+//     }
+//   );
+
+//   return (
+//     <>
+//       {/* con Tailwind */}
+//       <div className={filterclasses}>
+//         <a href="#" className="md:hidden">
+//           Todas las obras
+//         </a>
+
+//         <a href="#" className="hidden md:inline">
+//           Todo
+//         </a>
+//         <a href="#" className="hidden md:inline">
+//           {" "}
+//           Mes 1
+//         </a>
+//         <a href="#" className="hidden md:inline">
+//           {" "}
+//           Mes 2{" "}
+//         </a>
+//         <a href="#" className="hidden md:inline">
+//           Mes 3
+//         </a>
+
+//         <div className={filterIcon}>
+//             <BookingBtn text={"Fecha"} variant={"gray"} icon={"Calendar"} />
+//         </div>
+
+//         <div className={filterIcon}>
+//           <BookingBtn text={"Genero"} variant={"gray"} icon={"Genre"} />
+//         </div>
+//         <div className={filterIcon}>
+//           <BookingBtn text={"Asociacion"} variant={"gray"} icon={"Company"} />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default FilterBar;
+
+
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+//Cambiar por asociaciones en la api
+const people = [
+  {
+    id: 1,
+    name: "Wade Cooper",
+    avatar:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 2,
+    name: "Arlene Mccoy",
+    avatar:
+      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 3,
+    name: "Devon Webb",
+    avatar:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
+  },
+  {
+    id: 4,
+    name: "Tom Cook",
+    avatar:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 5,
+    name: "Tanya Fox",
+    avatar:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 6,
+    name: "Hellen Schmidt",
+    avatar:
+      "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 7,
+    name: "Caroline Schultz",
+    avatar:
+      "https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 8,
+    name: "Mason Heaney",
+    avatar:
+      "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 9,
+    name: "Claudie Smitham",
+    avatar:
+      "https://images.unsplash.com/photo-1584486520270-19eca1efcce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    id: 10,
+    name: "Emil Schaefer",
+    avatar:
+      "https://images.unsplash.com/photo-1561505457-3bcad021f8ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function FilterBar() {
+  const [selected, setSelected] = useState(people[3]);
 
   return (
-    <>
-      {/* con MediaQuery */}
-      {/*<div className={filterclasses}>
-        {/* <a href="#" className="all-works">
-          Todas las obras
-        </a>
+    <Listbox value={selected} onChange={setSelected}>
+      {({ open }) => (
+        <>
+          <Listbox.Label className="block text-base font-medium leading-6 text-slate-500 text-justify">
+            Asociación
+          </Listbox.Label>
+          <div className="relative mt-2">
+            <Listbox.Button className="relative w-50 cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+              <span className="flex items-center">
+                <img
+                  src={selected.avatar}
+                  alt=""
+                  className="h-5 w-5 flex-shrink-0 rounded-full"
+                />
+                <span className="ml-3 block truncate">{selected.name}</span>
+              </span>
+              <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                <ChevronUpDownIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
 
-        <a href="#" className="show-desktop">
-          Todo
-        </a>
-        <a href="#" className="show-desktop">
-          Mes 1
-        </a>
-        <a href="#" className="show-desktop">
-          Mes 2
-        </a>
-        <a href="#" className="show-desktop">
-          Mes 3
-        </a> */}
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {people.map((person) => (
+                  <Listbox.Option
+                    key={person.id}
+                    className={({ active }) =>
+                      classNames(
+                        active ? "bg-indigo-600 text-white" : "text-gray-900",
+                        "relative cursor-default select-none py-2 pl-3 pr-9"
+                      )
+                    }
+                    value={person}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <img
+                            src={person.avatar}
+                            alt=""
+                            className="h-5 w-5 flex-shrink-0 rounded-full"
+                          />
+                          <span
+                            className={classNames(
+                              selected ? "font-semibold" : "font-normal",
+                              "ml-3 block truncate"
+                            )}
+                          >
+                            {person.name}
+                          </span>
+                        </div>
 
-      {/* con Tailwind */}
-      <div>
-      {/* Filtros por mes: Todo | Octubre | Noviembre | Diciembre */}
-      <div className={filterclasses}>
-        
-        <a href="#" className="">
-          Todo
-        </a>
-        <a href="#" className="hidden md:inline">
-          {" "}
-          Octubre
-        </a>
-        <a href="#" className="hidden md:inline">
-          {" "}
-          Noviembre
-        </a>
-        <a href="#" className="hidden md:inline">
-          Diciembre
-        </a>
-
-        </div>
-
-        <div className="flex">
-
-        <div className={filterIcon}>
-            <BookingBtn text={"Fecha"} variant={"gray"} icon={"Calendar"} />
-        </div>
-
-        <div className={filterIcon}>
-          <BookingBtn text={"Genero"} variant={"gray"} icon={"Genre"} />
-        </div>
-        <div className={filterIcon}>
-          <BookingBtn text={"Asociacion"} variant={"gray"} icon={"Company"} />
-        </div>
-
-        {/* <BookingBtn className={filterBtn} text={"Fecha"} variant={"gray"} icon={"Calendar"} />
-        <BookingBtn className={filterBtn} text={"Genero"} variant={"gray"} icon={"Genre"} />
-        <BookingBtn className={filterBtn} text={"Asociacion"} variant={"gray"} icon={"Company"} /> */}
-      </div>
-      </div>
-    </>
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? "text-white" : "text-indigo-600",
+                              "absolute inset-y-0 right-0 flex items-center pr-4"
+                            )}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
   );
-};
-
-export default FilterBar;
+}
