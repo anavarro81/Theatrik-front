659ddef99bf0f8b83cb902aff2ebe79a0c59e761
yeray <yeray.programacion.web@gmail.com>
2024-04-02 13:43:36 +0100

    Home actualizado

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Slider from '../../components/organisms/Slider'
+import Headers from '../../components/organisms/Header';
+import FilterBar from '../../components/organisms/FilterBar';
 
 const Home = () => {
 
   return (
     <>
+      <Headers/>
       <Slider/>
+      <FilterBar/>
     </>
   )
 }
