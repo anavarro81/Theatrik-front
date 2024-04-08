862dfb0def9da2674e7e9f088b82d4d83a8a8fe9
antonio-dojedul <anavarro67@hotmail.com>
2024-04-08 13:36:26 +0200

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
-import React from "react";
-import Home from "./pages/Home/Home";
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import BookingPage from './pages/BookingPage/BookingPage'
+import Info from './pages/Info/Info'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+import Layout from './Layout/Layout'
+import Home from './pages/Home/Home'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <Home/>,
+        loader: async () => {
+          return fetch('http://localhost:5002/play/getAllPlays') 
+      }
+
+      },
+      
+      {
+        path: '/booking/:id',
+        element: <BookingPage/>,
+        loader: async ({params}) => {
+          return fetch(`http://localhost:5002/play/getPlay/${params.id}`)
+        }
+
+      },
+
+      {
+        path: '/info/:id',
+        element: <Info/>, 
+        loader: ({params}) => {
+          
+          return fetch(`http://localhost:5002/play/getPlay/${params.id}`)
+        }
+
+      }
+    
+    ]
+
+  },
+
+  
+  ]
+  
+  
+
+)
 
 const App = () => {
   return (
     <>
-      <Home/>
-    </>
-  );
-};
+    <RouterProvider router={router}/>
+    </>  
+)}
 
-export default App;
+export default App
