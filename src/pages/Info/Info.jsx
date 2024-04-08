b89553f0aef1646f682e37ac57f84600396123f9
antonio-dojedul <anavarro67@hotmail.com>
2024-04-08 14:33:42 +0200

    fix(front): :fire: get plays from info and booking components
    get plays from info and booking components

--- src/pages/Info/Info.jsx
+++ src/pages/Info/Info.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const Info = () => {
+  const play =  useLoaderData();
+  console.log(play);
+  
+  
   return (
     <div>Info</div>
   )
