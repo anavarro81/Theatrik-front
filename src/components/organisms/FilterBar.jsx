edcae0b5ae34d023cba52555eb0ae371f8dcb202
yeray <yeray.programacion.web@gmail.com>
2024-04-15 12:40:19 +0100

    responsive movil y escritorio

--- src/components/organisms/FilterBar.jsx
+++ src/components/organisms/FilterBar.jsx
@@ -1,5 +1,5 @@
 // import AsociationFilter from "../molecules/AsociationFilter";
-import React from "react";
+import React, { useState } from "react";
 import Links from "../molecules/Links";
 import CalendarFilter from "../molecules/CalendarFilter";
 import GenderFilter from "../molecules/GenderFilter";
@@ -8,36 +8,42 @@ import ButtonIconFilter from "../molecules/ButtonIconFilter";
 
 export default function FilterBar() {
 
+  const [visible, setVisible] = useState(false)
+
   return (
-    
-      // bg-green-700 flex pt-2 pb-2
-      <div className="">
-        <div className="bg-gray-200 hidden">
+    // bg-green-700 flex pt-2 pb-2
+    <div className="flex">
+      <div className="bg-gray-200 hidden md:block">
+        <div className="h-full flex items-center mr-6">
           <Links />
         </div>
+      </div>
 
-        {/* version movil */}
-        <div className="bg-gray-300 w-full flex justify-around">
-          <div>
-            <ButtonIconFilter icon={"Company"} text={"Asociación"} />
-          </div>
-          <div>
-            <ButtonIconFilter icon={"Calendar"} text={"Calendario"} />
-          </div>
-          <div>
-            <ButtonIconFilter icon={"Genre"} text={"Genero"} />
-          </div>
+      {/* version movil */}
+      <div className="bg-gray-300 w-full flex justify-around md:justify-end ">
+        <div className="md:mr-14">
+          <ButtonIconFilter
+            icon={"Company"}
+            text={"Asociación"}
+            onClick={() => setVisible(true)}
+          />
         </div>
+        <div className="md:mr-14">
+          <ButtonIconFilter icon={"Calendar"} text={"Calendario"} />
+        </div>
+        <div className="md:mr-20">
+          <ButtonIconFilter icon={"Genre"} text={"Genero"} />
+        </div>
+      </div>
 
-        {/* version escritorio */}
-        {/* <div className="hidden md:block bg-gray-300 justify-around flex-col items-center">
+      {/* version escritorio */}
+      {/* <div className="hidden md:block bg-gray-300 justify-around flex-col items-center">
         <div className="flex justify-around mt-4 ">
           <AsociationFilter />
           <CalendarFilter />
           <GenderFilter />
           </div>
         </div> */}
-      </div>
-
+    </div>
   );
 }
