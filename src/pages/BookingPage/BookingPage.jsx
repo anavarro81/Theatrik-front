b469ca0599ee71f4e21e6803ecc0fc59a6432f54
antonio-dojedul <anavarro67@hotmail.com>
2024-04-15 16:52:24 +0200

    feat(front): :art: Booking Page and Form Validation
    The reservation form with field validations is included.

--- src/pages/BookingPage/BookingPage.jsx
+++ src/pages/BookingPage/BookingPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
-
+import BookingForm from '../../components/organisms/BookingForm';
+import Image from '../../components/atoms/Image/Image';
+import Title from '../../components/atoms/Title/Title';
+import classNames from 'classnames';
 
 const BookingPage = () => {
 
@@ -9,8 +12,48 @@ const BookingPage = () => {
   
 
   return (
-    <div>BookingPage</div>
+    
+    
+
+    // <div className='flex justify-center ' >
+    //       <div className='flex flex-col mx-6' >
+    //       <BookingForm/>
+    //       </div>
+    //       <div className=''>
+    //         <Image url={play.cartel}/>
+    //       </div>
+    //   </div>
+      
+
+    // <div id='divider' className='flex justify-around w-full h-lvh '>
+    //   <div id='left-side' className='flex-1  h-full bg-green-100'>
+        
+    //     <div className='flex flex-col mx-6'>
+    //     <Title type={"H1"} text={"Reservar "} />
+    //     <BookingForm/>        
+    //     </div>
+    //   </div>
+    //   <div id='right-side' className='flex-1 h-full bg-blue-300'>
+    //   <Image url={play.cartel} />
+    //   </div>
+    // </div>
+
+  <div className='flex flex-col-reverse md:flex-row md:justify-around  m-2 '>
+
+    <div className='flex w-full space-y-4 md:flex flex-col md:w-[60%]'> 
+      <Title type={"H1"} text={"Reservar "} />
+      <BookingForm/> 
+     </div> 
+
+    <div className='w-full bg-red-100 md:w-[30%]'>
+    <Image url={play.cartel} />
+    </div>
+
+  </div>
+
+
   )
 }
 
 export default BookingPage
+
