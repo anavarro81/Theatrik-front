1766a58fdbb8d5f7fa2895aad606369e547423f6
yeray <yeray.programacion.web@gmail.com>
2024-04-19 12:28:20 +0100

    cambios 4

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />,
         loader: async () => {
-          return fetch("http://localhost:5000/play/getAllPlays");
+          return fetch("http://localhost:5004/play/getAllPlays");
         },
       },
 
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         path: "/booking/:id",
         element: <BookingPage />,
         loader: async ({ params }) => {
-          return fetch(`http://localhost:5000/play/getPlay/${params.id}`);
+          return fetch(`http://localhost:5004/play/getPlay/${params.id}`);
         },
       },
 
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
         path: "/info/:id",
         element: <Info />,
         loader: ({ params }) => {
-          return fetch(`http://localhost:5000/play/getPlay/${params.id}`);
+          return fetch(`http://localhost:5004/play/getPlay/${params.id}`);
         },
       },
     ],
