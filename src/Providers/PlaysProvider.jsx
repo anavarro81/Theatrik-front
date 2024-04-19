000a7450819aba295457e2d3d1b5c0736ddf758c
antonio-dojedul <anavarro67@hotmail.com>
2024-04-19 11:56:23 +0200

    subir

--- src/Providers/PlaysProvider.jsx
+++ src/Providers/PlaysProvider.jsx
@@ -10,28 +10,19 @@ function PlaysProvider ({ children }) {
     const getPlays = () => {
         fetch('http://localhost:5002/play/getAllPlays')
         .then((res) => res.json())
-        .then((data) => setPlays(data))
-        
-        
+        .then((data) => setPlays(data))       
       }
 
-    const searchPlay = (searchParams) => {
-        
-        
-
+    const searchPlay = (searchParams) => {       
         const {searchTerm, searchFor} = searchParams
-
         
         const result = plays.filter((play) => {
             return play[searchFor].includes(searchTerm)
           })
-
       
           setFilteredPlays(result)
-
     }
     
-
     return (
         <PlaysContext.Provider value={[plays, getPlays, filteredPlays, searchPlay]}>
             { children }
