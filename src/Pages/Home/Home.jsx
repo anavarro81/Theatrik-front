a1cb1c8c0eba3eb43e559e2d13a4909e062fcc90
yeray <yeray.programacion.web@gmail.com>
2024-04-22 10:08:31 +0100

    cambios actuales

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,23 +7,6 @@ const Home = () => {
 
   const plays =  useLoaderData();
 
-<<<<<<< HEAD:src/Pages/Home/Home.jsx
-  //useState
-  const handleFilterPlays = (filterPlays) =>{
-
-  }
-
-  return (
-    <>
-    
-      <Slider />
-      <FilterBar plays={plays} />
-      <PlayGallery plays={plays} />
-      
-    </>
-  );
-}
-=======
   const sliderPlays = plays.slice(0,5)
 
   return (
@@ -36,6 +19,5 @@ const Home = () => {
     </>
   )
 };
->>>>>>> 041175e6dd4fbbf41c12a9adf72de5b72fe3a7a7:src/pages/Home/Home.jsx
 
 export default Home;
