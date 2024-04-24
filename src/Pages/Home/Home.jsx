e90b95420a1b5602bcb565d3ee128c1093d76a06
sarardguezhe <sara.rodriguezhe@gmail.com>
2024-04-24 10:58:51 +0200

    fecha para ejemplo

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,11 +9,13 @@ const Home = () => {
   const plays =  useLoaderData();
   
   const [sliderPlays, setSliderPlays] = useState([]);
-  const currentDate = (new Date()); 
+  const currentDate = (new Date("2023-11-05")); 
 
   const updateSlider = () => {
     
     const filteredPlays = plays?.filter(play => new Date(play.date) >= currentDate).slice(0,5);
+
+    console.log(filteredPlays);
 
     setSliderPlays(filteredPlays);
 
