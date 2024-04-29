4b069d6551b4aee16b859a2e4cd91ed0d9735ed0
antonio-dojedul <anavarro67@hotmail.com>
2024-04-29 11:45:38 +0200

    fix: include provider in app.jsx
    include provider in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Info from './Pages/Info/Info'
 import ErrorPage from './Pages/ErrorPage/ErrorPage'
 import Layout from './Layout/Layout'
 import Home from './Pages/Home/Home'
-
+import SearchResultsPage from './Pages/SearchResultsPage/SearchResultsPage'
+import { PlaysProvider } from './Providers/PlaysProvider'
 
 const router = createBrowserRouter([
   {
@@ -36,6 +37,12 @@ const router = createBrowserRouter([
           return fetch(`http://localhost:5002/play/getPlay/${params.id}`);
         },
       },
+
+      {
+        path: "/SearchResultsPage",
+        element: <SearchResultsPage/>
+      },
+
     ],
   },
 ]);
@@ -43,8 +50,9 @@ const router = createBrowserRouter([
 const App = () => {
   return (
     <>
-    
+      <PlaysProvider>
       <RouterProvider router={router} />
+      </PlaysProvider>
     </>
   );
 };
