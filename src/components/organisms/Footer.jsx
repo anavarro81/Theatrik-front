6c932876d106c2d92018ab7e8ef08bbd109bf914
sarardguezhe <sara.rodriguezhe@gmail.com>
2024-04-29 12:27:57 +0200

    footer updated

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-end h-12 w-full bg-black pr-20 mt-2 text-white text-right	px-2 fixed bottom-0 ">
+    <div className="flex items-center justify-end h-12 w-full bg-black pr-20 mt-2 text-white text-right	px-2">
       {" "}
       Copyright &#169; 2024 | Creado por...{" "}
     </div>
