871de6b0ec189fd081082e4d69f5743d84c39e9b
antonio-dojedul <anavarro67@hotmail.com>
2024-04-29 12:21:22 +0200

    feat: :art: upload header
    upload header

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import SearchBar from '../molecules/SearchBar'
 import Icon from '../atoms/Icon/Icon'
+import { usePlays } from '../../Providers/PlaysProvider'
+import { useState } from 'react'
+import {useLocation, Link} from "react-router-dom";
+
+
 
 const Header = ({placeholder}) => {
+
+  let location = useLocation();
+  console.log(location);
   
+  const [plays, getPlays, filteredPlays, searchPlay, multipleSearch] = usePlays();
+
+  const [searchString, setSearchString] = useState("")
+    
   const handleSearch = () => {
-    console.log('Estoy buscando...');
+    console.log('Estoy en handleSearch');    
+    multipleSearch(searchString)
+
   }
+
+
+
   
   return (
     <>
@@ -14,11 +31,12 @@ const Header = ({placeholder}) => {
     <div className='flex flex-row justify-between px-4 mb-4'>
     <Icon type={"Logo"}/>
     <div className='space-x-4 m-2 flex' >      
-      <input type="text" placeholder='Obra o asociación' className="border p-3 w-full"/>
+      <input type="text" placeholder='Obra o asociación' className="border p-3 w-full" onChange={() => setSearchString(event.target.value)}/>
       <button onClick={handleSearch}>
       <Icon type={"Search"}/>
       </button>
-      <Icon type={"Burger"}/>
+      { location.pathname != '/' && <Link to={'/'} > <Icon type={"House"}/> </Link> }
+ 
     </div>
     </div>
     </>
