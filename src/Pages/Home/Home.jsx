6c35cc1256f33a119ca7ba446c54ff0fcc21c3eb
yeray <yeray.programacion.web@gmail.com>
2024-04-30 17:07:47 +0100

    hay que mirar el home para evitar conflictos con el de sara

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -52,3 +52,43 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+// import HomeSlider from '../../components/organisms/Slider';
+// import FilterBar from '../../components/organisms/FilterBar';
+// import PlayGallery from '../../components/organisms/PlayGallery';
+// import { useEffect, useState } from 'react';
+// import { useLoaderData } from 'react-router-dom';
+
+// const Home = () => {
+
+//   const plays =  useLoaderData();
+
+//   const [sliderPlays, setSliderPlays] = useState([]);
+//   const currentDate = (new Date("2023-11-05")); // Modificar por (new Date()) para obtener la fecha actual
+
+//   const updateSlider = () => {
+
+//     const filteredPlays = plays?.filter(play => new Date(play.date) >= currentDate).slice(0,5);
+
+//     setSliderPlays(filteredPlays);
+
+//   };
+
+//   useEffect(() => {
+//     updateSlider(); 
+//   }, []);
+
+//   return (
+//     <>
+
+//       <HomeSlider data={sliderPlays}/>
+//       <FilterBar/>
+//       <PlayGallery plays={plays}/>
+
+//     </>
+//   )
+// };
+
+// export default Home;
