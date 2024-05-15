d8b7f55ea318482b06f68c5a228c1baf96701313
JEDI84 <yerayarmas84@gmail.com>
2024-05-15 19:13:31 +0100

    info reconfigured

--- src/Pages/Info/Info(OriginalCambiarNombre).jsx
+++ src/Pages/Info/Info(OriginalCambiarNombre).jsx
@@ -13,42 +13,41 @@ const Info = () => {
   const play = useLoaderData();
   const company = play.company[0];
 
-  const h2 = "text-[14px] mb-px ml-1";
+  const h2 = "text-1 mb-px ml-1";
 
   return (
-    <div className="min-h-screen">
-      <div className="parent p-2 border border-black border-solid rounded-lg mt-10 flex items-center justify-center inline-block max-w-5xl m-auto flex-col md:flex-row">
-        <div className="left mb-10 m-auto pl-3 pr-3 flex-col w-88 sm:w-112 break-all">
-          <h1 className="text-[22px] leading-4 mb-2 mt-2">
+    <div className="min-h-screen ">
+      <div className="parent p-2 border border-black border-solid rounded-lg mt-10 flex items-center justify-center inline-block max-w-2xl m-auto flex-col md:flex-row">
+        <div className="left mb-10 m-auto pl-4 pr-4 flex-col w-72 sm:w-112 break-all">
+          <h2>
             <b>{play.title}</b>
-          </h1>
+          </h2>
+          <h3 className="text-[12px] leading-4 mb-1">{play.synopsis}</h3>
 
-          <h3 className="text-[16px] leading-4 mb-4">{play.synopsis}</h3>
-
-          <h3 className="text-[18px] leading-3 mb-2">
+          <h3 className="text-xs leading-3 mb-1">
             <b>Intérpretes:</b>
           </h3>
-          <h4 className="mb-1 text-[14px] mb-4">{play.actors}</h4>
+          <h4 className="mb-1 text-[10px] mb-1">{play.actors}</h4>
 
-          <h3 className="text-[18px] leading-3 mb-2">
+          <h3 className="text-xs leading-3 mb-1">
             <b>Dirección de escena avión:</b>
           </h3>
-          <h4 className="mb-1 text-[14px] mb-4">{play.director}</h4>
+          <h4 className="mb-1 text-[10px] mb-1">{play.director}</h4>
 
-          <h3 className="text-[18px] leading-3 mb-2">
+          <h3 className="text-xs leading-3 mb-1">
             <b>Escenografía y vestuario:</b>
           </h3>
-          <h4 className="mb-1 text-[14px] mb-4">{play.company_name}</h4>
+          <h4 className="mb-1 text-[10px] mb-1">{play.company_name}</h4>
 
-          <h3 className="text-[18px] leading-3 mb-2">
+          <h3 className="text-xs leading-3 mb-1">
             <b>Cartel:</b>
           </h3>
-          <h4 className="mb-1 text-[14px] mb-4 whitespace-break-spaces">
+          <h4 className="mb-1 text-[10px] mb-1 whitespace-break-spaces">
             {play.cartel}
           </h4>
 
-          <div className="box border-sm border border-black rounded-lg w-60 h-auto mx-auto text-[9px] p-1 mt-4">
-            <p className="font-bold inline ml-1 text-[16px]">
+          <div className="box border-sm border border-black rounded-lg w-52 h-auto mx-auto text-[9px] p-1 mt-4">
+            <p className="font-bold inline ml-1 text-xs">
               Acerca de {play.company_name}
             </p>
             <h2 className={h2}>
@@ -78,14 +77,14 @@ const Info = () => {
           </div>
         </div>
 
-        <div className="right m-auto pr-2 pl-4 flex-col lg:w-160 mb-22">
+        <div className="right m-auto pr-2 pl-4 flex-col w-72 sm:w-112">
           <img
             src={play.cartel}
-            className="rounded-2xl h-full mt-2 min-w-80"
+            className="rounded-2xl h-full mt-2 min-w-52"
           ></img>
-          <div className="box2 border-sm border border-black rounded-lg w-60 h-auto mx-auto text-[9px] p-1 mt-40 ">
+          <div className="box2 border-sm border border-black rounded-lg w-52 h-auto mx-auto mb-8 text-[9px] p-1 mt-10 ">
             <div>
-              <p className="font-bold inline text-[16px] ml-1">Title:</p>
+              <p className="font-bold inline text-xs ml-1">Title:</p>
               <h2 className={h2}>
                 <img
                   src={MapIcon}
@@ -114,7 +113,7 @@ const Info = () => {
                 </h2>
                 <Link
                   to={`/booking/${play._id}`}
-                  className="bg-orange-500 text-[14px] w-20 h-6 hover:bg-blue-700 text-white font-bold flex items-center justify-center py-2 px-2 rounded ml-20"
+                  className="bg-orange-500 w-14 h-5 hover:bg-blue-700 text-white font-bold flex items-center justify-center py-2 px-2 rounded ml-20"
                 >
                   Reservar
                 </Link>
