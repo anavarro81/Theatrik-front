5361664a3f5087e09da4dde2dd977793d29b6249
anavarro81 <87661508+anavarro81@users.noreply.github.com>
2024-07-03 17:11:57 +0200

    Incluir icono lupa dentro del buscador (#37)

--- src/Pages/Info/Info.jsx
+++ src/Pages/Info/Info.jsx
@@ -7,6 +7,7 @@ import SmartphoneIcon from "../../components/atoms/Icon/smartphone.svg";
 import MapIcon from "../../components/atoms/Icon/map.svg";
 import CalendarDateIcon from "../../components/atoms/Icon/calendar-date.svg";
 import ClockIcon from "../../components/atoms/Icon/clock.svg";
+import Icon from "../../components/atoms/Icon/Icon"
 import "./styles.css";
 
 const Info = () => {
@@ -16,116 +17,113 @@ const Info = () => {
   const h2 = "text-[20px] mb-px ml-1";
 
   return (
-    <div className="min-h-screen ">
-      <div className="parent p-2  rounded-lg mt-10 flex items-center justify-center inline-block max-w-screen-2xl m-auto flex-col md:flex-row">
-        <div className="left mb-18 m-auto pl-20 pr-20 flex-col w-3/4 sm:w-112 break-all">
-          <h2 className="text-[32px]">
-            <b>{play.title}</b>
-          </h2>
-          <h3 className="text-[18px] leading-2 mb-4 ">{play.synopsis}</h3>
+    <>
 
-          <h3 className="text-[22px] leading-3 mb-2">
-            <b>Intérpretes:</b>
-          </h3>
-          <h4 className="mb-1 text-[18px] mb-4">{play.actors}</h4>
+<div className='flex flex-col lg:flex-row justify-center items-center lg:items-start p-5'>
+      <div className='lg:w-1/2 p-4 order-2 lg:order-1'>
+{/* Content Sections */}
+        <div className='space-y-4'>         
+          <section>
+            <h2 className='font-bold text-xl'> {play.title} </h2>
+            <p> {play.synopsis} </p>
+          </section>
 
-          <h3 className="text-[22px] leading-3 mb-2">
-            <b>Dirección de escena:</b>
-          </h3>
-          <h4 className="mb-1 text-[18px] mb-4">{play.director}</h4>
+          <section>
+            <h2 className='font-bold text-xl'> Interpretes </h2>
+            <p> {play.actors} </p>
+          </section>
 
-          <h3 className="text-[22px] leading-3 mb-2">
-            <b>Escenografía y vestuario:</b>
-          </h3>
-          <h4 className="mb-1 text-[18px] mb-4">{play.company_name}</h4>
-
-          <h3 className="text-[22px] leading-3 mb-2">
-            <b>Cartel:</b>
-          </h3>
-          <h4 className="mb-1 text-[18px] mb-4 whitespace-break-spaces">
-            {play.cartel}
-          </h4>
-
-          <div className="box border-sm border border-black rounded-lg w-72 h-auto mx-auto text-[9px] p-1 mt-8">
-            <p className="font-bold inline ml-1 text-[20px]">
-              Acerca de {play.company_name}
-            </p>
-            <h2 className={h2}>
-              <img
-                src={GlobeIcon}
-                alt="Globe icon"
-                className="inline-block h-5 w-6 mr-2"
-              />
-              {company.web}
-            </h2>
-            <h2 className={h2}>
-              <img
-                src={MailIcon}
-                alt="Mail icon"
-                className="inline-block h-5 w-6 mr-2"
-              />
-              {company.email}
-            </h2>
-            <h2 className={h2}>
-              <img
-                src={SmartphoneIcon}
-                alt="Smartphone icon"
-                className="inline-block h-5 w-6 mr-2"
-              />
-              {company.phone}
-            </h2>
-          </div>
+          <section>
+            <h2 className='font-bold text-xl'> Director </h2>
+            <p>{play.director} </p>
+          </section>        
+          
         </div>
 
-        <div className="right m-auto pr-2 pl-4 flex-col w-96 mb-22">
-          <img
-            src={play.cartel}
-            className="rounded-2xl h-full mt-4 min-w-80"
-          ></img>
-          <div className="box2 border-sm border border-black rounded-lg w-90 h-auto mx-auto text-[20px] p-1 mt-4 ">
-            <div>
-              <p className="font-bold inline text-[20px] ml-1">Title:</p>
-              <h2 className={h2}>
-                <img
-                  src={MapIcon}
-                  alt="Map icon"
-                  className="inline-block h-5 w-6 mr-2"
-                />
-                {play.place}
-              </h2>
-              <h2 className={h2}>
-                <img
-                  src={CalendarDateIcon}
-                  alt="Calendar icon"
-                  className="inline-block h-5 w-6 mr-2"
-                />
-                {play.date.substring(0, 10)}
-              </h2>
-
-              <div className="flex items-center">
-                <h2 className={h2}>
-                  <img
-                    src={ClockIcon}
-                    alt="Clock icon"
-                    className="inline-block h-5 w-6 mr-2"
-                  />
-                  <span>{play.time}</span>
-                </h2>
-                <Link
-                  to={`/booking/${play._id}`}
-                  className="bg-orange-500 text-[18px] w-32 h-8 hover:bg-blue-700 text-white font-bold flex items-center justify-center py-2 px-2 rounded ml-32"
-                >
-                  Reservar
-                </Link>
-              </div>
-            </div>
-            <div></div>
-          </div>
+        <h3 className='text-2xl font-semibold pt-8 pb-4'> Mas sobre {play.company_name}</h3>
+        <div className='flex space-x-4 items-center'>
+          {/* Incluir iconos */}
+          <a href='mailto:email@example.com' className=''> <Icon type="Glove"></Icon> </a>
+          <a href='mailto:email@example.com' className=''> <Icon type="Mail"></Icon> </a>
+          <a href='tel:+1234567890' className=''> <Icon type="Smartphone"></Icon></a>
         </div>
       </div>
+      <div className='lg:p-4 order-1 lg:order-2'>
+        
+        <img src={play.cartel} alt='Placeholder' className='max-w-full h-auto object-cover lg:max-w-[465px]' />
+        
+        {/* Place and hour info*/}
+        <div className="hidden lg:block order-3 lg:w-full  "> 
+        <div id='placeInfo' className='mt-4 border p-4 rounded-lg shadow-lg '>
+              
+              {/* Place */ }
+              <div id='place' className='flex items-center space-x-2 '>
+                <Icon type={"Map"}/>
+                <p> {play.place} </p>
+              </div>
+
+              {/* Date */}
+              <div className='flex items-center space-x-2 my-2 '>
+                <Icon type={"Calendar_Yellow"}/>
+                <p> {play.date.substring(0, 10)} </p>
+              </div>
+
+              {/* Time and booking button */ }
+              <div className='w-full items-center lg:flex lg:justify-between space-x-2 '>
+                <Icon type={"Clock"}/>
+                <p> {play.time} </p>          
+                <div className='mt-2  w-full lg:flex lg:justify-end'>
+                <Link to={`/booking/${play._id}`}> 
+                  <button className='bg-orangeDesign  text-white font-bold py-2 px-4 rounded-full inline-block h-[42px] w-full'> Reservar </button>
+                </Link>
+              </div>      
+          </div>
+
+        </div> 
+      
+      </div>  
+              
+      </div>
+      
+      {/* Place and hour info*/}
+      <div className="w-full lg:hidden order-3 "> 
+        <div id='placeInfo' className='mt-4 border p-4 rounded-lg shadow-lg '>
+              
+              {/* Place */ }
+              <div id='place' className='flex items-center space-x-2 '>
+                <Icon type={"Map"}/>
+                <p> {play.place} </p>
+              </div>
+
+              {/* Date */}
+              <div className='flex items-center space-x-2 my-2 '>
+                <Icon type="Calendar_Yellow"/> 
+                <p> {play.date.substring(0, 10)} </p>
+              </div>
+
+              {/* Time and booking button */ }
+              <div className='w-full items-center lg:flex lg:justify-between space-x-2 '>
+                <div className="flex items-center space-x-2"> 
+                  <Icon type="Clock"/>                
+                  <p> {play.time} </p>          
+                </div>
+                <div className='mt-2  w-full lg:flex lg:justify-end'>
+                
+                <Link to={`/booking/${play._id}`}> 
+                  <button className='bg-orangeDesign  text-white font-bold py-2 px-4 rounded-full inline-block h-[42px] w-full'> Reservar </button>
+                </Link>
+              </div>      
+          </div>
+
+        </div> 
+      
+      </div>   
     </div>
-  );
-};
 
+  
 
+    </>
+  )
+}
 export default Info;
+
