528a6b9433b8e4675db9f460c155b44a679f225e
Antonio Navarro <anavarro67@hotmail.com>
2024-07-08 19:12:44 +0200

    feat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import BookingPage from './Pages/BookingPage/BookingPage'
-import Info from './Pages/InfoPage/InfoPage'
+import InfoPage from './Pages/InfoPage/InfoPage'
 import ErrorPage from './Pages/ErrorPage/ErrorPage'
 import Layout from './Layout/Layout'
 import Home from './Pages/Home/Home'
@@ -52,7 +52,7 @@ const router = createBrowserRouter([
 
       {
         path: "/info/:id",
-        element: <Info />,
+        element: <InfoPage />,
         
         // loader: ({ params }) => {          
         //   //return fetch(`http://localhost:5002/play/getPlay/${params.id}`);
